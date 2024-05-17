297bab2beabb5b4c76d2eeeea33c97f034952437
ZKWolf <34097612+wolfswolke@users.noreply.github.com>
2024-05-17 22:14:36 +0200

    Added error codes, LOG ids (Random atm) and changed log data required

--- src/endpoints/logging.py
+++ src/endpoints/logging.py
@@ -1,17 +1,21 @@
+import random
+
 from flask_definitions import *
 
 
 @app.route('/api/v1/log', methods=["POST"])
 def log():
     check_user_agent("soft")
+    random_id = random.randint(1, 10000000)
     try:
         data = request.json
         logger.graylog_logger(level="info", handler="log_upload", message=data)
-        return jsonify({"status": "success"})
+        return jsonify({"status": "success", "random_id": random_id}), 200
     except TimeoutError:
-        return jsonify({"status": "error"})
+        return jsonify({"status": "error"}), 500
     except Exception as e:
         logger.graylog_logger(level="error", handler="general-log", message=e)
+        return jsonify({"status": "UNKNOWN ERROR"}), 500
 
 
 @app.route("/api/v1/bug_report", methods=["POST"])
@@ -27,9 +31,10 @@ def bug_report():
                "Bug Report",
                f"Reporter: {data['Reporter']}\nDescription: "
                f"{data['Description']}\nSteps: {data['Steps']}\nExpected: "
-               f"{data['Expected']}\nDiscord: {data['Discord']}\nEmail: {data['Email']}")
+               f"{data['Expected']}\nContact: {data['contact']}\nLogID: {data['log_id']}")
         return jsonify({"status": "success"})
     except TimeoutError:
-        return jsonify({"status": "error"})
+        return jsonify({"status": "error"}), 500
     except Exception as e:
         logger.graylog_logger(level="error", handler="general-bug_report", message=e)
+        return jsonify({"status": "UNKNOWN ERROR"}), 500
